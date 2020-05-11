7330ee03723a8a17057ee0cb0f2c2631bf883cd6
haeyong Hahn <youg1322@naver.com>
2020-05-11 14:58:36 +0900

    lifecircle & API 데이터 통신

--- emaillist-app/src/EmaillistApp.js
+++ emaillist-app/src/EmaillistApp.js
@@ -6,16 +6,45 @@ export default class EmaillistApp extends React.Component {
     constructor(){
         super(...arguments);
         this.state = {
-            keyword: ''
+            keyword: '',
+            emails: null
         }
+
+        console.log("EmaillistApp:Constructor()");
+    }
+
+    componentWillMount() {
+        console.log("EmaillistApp:componentWillMount()");
+    }
+
+    onNotifyKeywordChange(keyword) {
+        this.setState({
+            keyword: keyword
+        }) 
     }
 
     render() {
+        console.log("EmaillistApp:render()");
         return (
             <div className='EmaillisApp'>
-                <SearchBar />
-                <Emaillist keyword={ this.state.keyword } emails={ this.props.emails }/>
+                <SearchBar keyword={ this.state.keyword } notifyKeywordChangeHandler={ this.onNotifyKeywordChange.bind(this) } />
+                <Emaillist keyword={ this.state.keyword } emails={ this.state.emails }/>
             </div>
         );
+    }
+
+    componentDidMount() {
+        console.log("EmaillistApp:componentDidMount()");
+        fetch('http://localhost:8888/data.json')
+            .then(response => response.json())
+            .then(json => this.setState({
+                emails: json
+            }))
+            .catch((error) => console.log(error));
+    }
+
+    // 자원정리 해제할 때 여기서 정의한다.
+    componentWillUnmount() {
+        console.log("EmaillistApp:componentWillUnmount()");
     }
 }
