9e348c3ccba3e7e22c4fe157883a73bc76a3629b
haeyong Hahn <youg1322@naver.com>
2020-05-12 10:32:34 +0900

    kanban-app 깔끔 정리

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -2,11 +2,12 @@
 import React from 'react';
 import KanbanBoard from './KanbanBoard';
 import data from './data.json';
+import styles from './App.css'
 
 export default class App extends React.Component {
     render() {
         return (
-            <div className="App">
+            <div className={styles.App}>
                 <KanbanBoard cards={ data } />
             </div>
         )
--- kanban-app/src/TaskList.js
+++ kanban-app/src/TaskList.js
@@ -1,22 +1,30 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './TaskList.css';
 
 export default class TaskList extends React.Component {
     render() {
-        let taskComponents = [];
-        this.props.tasks.forEach(task => taskComponents.push(<li className='TaskList__Task'>
-            <input type='checkbox' defaultChecked={ task.done } />
-            { task.name }
-            <a href='#' className='TaskList__Task--remove' />
-        </li>));
-
+    //     let taskComponents = [];
+    //     this.props.tasks.forEach(task => taskComponents.push(<li className={ styles.Task }>
+    //         <input type='checkbox' defaultChecked={ task.done } />
+    //         { task.name }
+    //         <a href='#' className={ styles['Task--remove'] } />
+    //     </li>));
         return (
-            <div className='TaskList'>
+            <div>
                 <ul>
-                    { taskComponents }
+                    { this.props.tasks.map(task => 
+                        <li key={ task.id } className={ styles.Task }>
+                            <input type='checkbox' defaultChecked={ task.done } />
+                            { task.name }
+                            <a href='#' className={ styles['Task--remove'] } />
+                        </li> ) 
+                    }
                 </ul>
-                <input type='text' className='TaskList--add-task' placeholder='새태스크' />
+                <input 
+                    type='text' 
+                    className={ styles['Input--add-task'] } 
+                    placeholder='새태스크' />
              </div>
         )
     }
